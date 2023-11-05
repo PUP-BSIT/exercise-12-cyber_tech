17db1b76f91772571bddda19690ab29c819e9982
Kyla Rica Malaluan <malaluankylarica@gmail.com>
2023-11-05 11:52:36 +0800

    add changes to function

--- malaluan_kyla/kyla_malaluan_script.js
+++ malaluan_kyla/kyla_malaluan_script.js
@@ -3,18 +3,18 @@ let nameInput = document.getElementById("name");
 let commentInput = document.getElementById("comment");
 let commentsList = document.getElementById("comments_list");
 
-commentForm.addEventListener("submit", function (event) {
+commentForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
+  
     let name = nameInput.value;
     let comment = commentInput.value;
-
+  
     if (name && comment) {
-        let newComment = document.createElement("li");
-        newComment.textContent = `${name}: ${comment}`;
-        commentsList.appendChild(newComment);
-
-        nameInput.value = "";
-        commentInput.value = "";
+      let newComment = document.createElement("li");
+      newComment.textContent = `${name}: ${comment}`;
+      commentsList.appendChild(newComment);
+  
+      nameInput.value = "";
+      commentInput.value = "";
     }
-});
+});  
